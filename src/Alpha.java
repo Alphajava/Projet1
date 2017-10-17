176a63abab255a919121e5d719b80a612a7b9b93
Diallo <mamadou_alpha.diallo@yahoo.fr>
2017-10-17 16:26:06 +0200

    commit2

--- src/Alpha.java
+++ src/Alpha.java
@@ -3,51 +3,17 @@ import java.util.Arrays;
 public class Alpha {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-       // System.err.println("Hello word");
-		String A = "Bonjour";
-		String B = " à tous";
-		String tab1[] = {A, B};
-		//String tab2[];
-		System.out.println(A + B);
 		
-		System.out.print(tab1[0]);
-		System.out.println(tab1[1]);
-		
-		String[] tab2 = {"Bonjour ","à tous"};
-		System.out.println(tab2[0]+tab2[1]);
+		String tab[] = {"Tu as ", " ans"};
+		int age = 18;
 		
 		
-		
-		System.out.println(Arrays.toString(tab2));
-		
-		
-		
-		String X =(Arrays.toString(tab2));
-		System.out.println(X);
-		
-			
-		
-		
-		for(int i = 0; i < tab1.length; i++) 
-			
-		{
-			System.out.println(i);
-			System.out.println(tab1[i]);
-		}
-		
-		
-	
-		for (String variable : tab1) 
-		{
-			System.out.print(variable);
-		}
-		
-			
+		System.out.print(tab[0] + age + tab[1]);  // affichage 
 	}
-
-	
+}
 		
-	}
-
-
+		
+		
+		
+		
+		
